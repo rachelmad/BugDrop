651b78dd04fac876929c0030dbc1021888b9eb2a
rachelmad <rachelvmadrigal@gmail.com>
2015-03-21 11:45:55 -0500

    Created BugSet class and tests

--- bugdrop.py
+++ bugdrop.py
@@ -1,32 +1,56 @@
-GAMEWIDTH = 6
-GAMEHEIGHT = 12
+import random
+
+NUMGAMECOLUMNS = 6
+COLUMNHEIGHT = 12
+
 
 class BugDrop(object):
     def __init__(self):
-        self.gameGrid = [[0 for x in range(GAMEHEIGHT)] for x in range(GAMEWIDTH)]      #grid = list of columns
-        self.base = [0 for x in range(GAMEWIDTH)]
+        self.gameGrid = [[0 for x in range(COLUMNHEIGHT)] for x in range(NUMGAMECOLUMNS)]      
+        self.base = [0 for x in range(NUMGAMECOLUMNS)]
     
     
     def addToColumn(self, color, column):
-        if column < GAMEWIDTH or column > 0:
+        if column < NUMGAMECOLUMNS and column >= 0:
             self.gameGrid[column][self.base[column]] = color
         else:
             raise IndexError('Column does not exist')
             
             
     def isAtMax(self, column):
-        if self.base[column] == GAMEHEIGHT - 1:
+        if self.base[column] == COLUMNHEIGHT - 1:
             return True
         return False
         
     
-    def updateBase(self):
+    def updateBase(self):                                       #change updatebase to get column base
         for column in range(len(self.gameGrid)):
             for row in range(len(self.gameGrid[column])):
                 if self.gameGrid[column][row] != 0:
                     self.base[column] = row + 1
+                    
+                    
+    def dropBugSetInColumn(self, bugSet, column):
+        if bugSet.position == 'up':
+            self.addToColumn(bugSet.bugColor1, column)
+            self.addToColumn(bugSet.bugColor2, column)
+            self.addToColumn(bugSet.bugColor3, column)
 
+
+class BugSet(object):
+    def __init__(self):
+        self.bugColor1 = random.randint(1, 5)
+        self.bugColor2 = random.randint(1, 5)
+        self.bugColor3 = random.randint(1, 5)
+        self.position = 'up'
+        self.location = (-1, -1)
         
+        
+    def setPosition(self, newPosition):
+        if newPosition == 'up' or newPosition == 'down' or newPosition == 'left' or newPosition == 'right':
+            self.position = newPosition
+        else:
+            raise ValueError('Invalid position')
     
 
     
--- testbugdrop.py
+++ testbugdrop.py
@@ -4,35 +4,54 @@ from bugdrop import *
 class BugDropTest(unittest.TestCase):
     def setUp(self): 
         self.bugDrop = BugDrop()
-        
+        self.bugSet = BugSet()        
         
     def testcanary(self):
-        self.assertTrue(True)
-        
+        self.assertTrue(True)        
         
     def testAddToColumnChangesGameGrid(self):
         self.bugDrop.addToColumn(1, 0)
-        self.assertEquals(1, self.bugDrop.gameGrid[0][0])
-        
+        self.assertEquals(1, self.bugDrop.gameGrid[0][0])        
     
     def testAddToColumnForInvalidColumnRaisesException(self):
-        self.assertRaises(IndexError, self.bugDrop.addToColumn(1, -1))
-        
+        self.assertRaises(IndexError, self.bugDrop.addToColumn, 1, -1)       
         
     def testIsAtMaxOnMaxedBaseReturnsTrue(self):
         self.bugDrop.base[0] = 11       #GAMEHEIGHT - 1
-        self.assertTrue(self.bugDrop.isAtMax(0))
-        
+        self.assertTrue(self.bugDrop.isAtMax(0))        
     
     def testIsAtMaxOnLowBaseReturnsFalse(self):
         self.bugDrop.base[0] = 0
         self.assertFalse(self.bugDrop.isAtMax(0))
-        
     
     def testAddToColumnIncreasesBase(self):
         self.bugDrop.addToColumn(1, 0)
         self.bugDrop.updateBase()
-        self.assertEquals(1, self.bugDrop.base[0])
+        self.assertEquals(1, self.bugDrop.base[0])        
+        
+    def testDropUpBugSetInColumnChangesGameGrid(self):
+        self.bugDrop.dropBugSetInColumn(self.bugSet, 1)
+        self.assertTrue(self.bugDrop.gameGrid[1][2] != 0)
+        
+        
+class BugSetTest(unittest.TestCase):
+    def setUp(self):
+        self.bugSet = BugSet()
+        
+    def testCanary(self):
+        self.assertTrue(True)
+        
+    def testInitialBugSetCreatesRandomSets(self):
+        random = BugSet()
+        self.assertTrue(self.bugSet != random)
+        
+    def testSetPositionChangesPositionForValidValue(self):
+        self.bugSet.setPosition('down')
+        self.assertEquals('down', self.bugSet.position)
+        
+    def testSetPositionThrowsErrorsForInvalidPositionValue(self):
+        self.assertRaises(ValueError, self.bugSet.setPosition, 'random')
+
         
 if __name__ == '__main__':
     unittest.main()
