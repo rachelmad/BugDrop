155b89a68d6a3a647d7747965d73e923ed1c0306
rachelmad <rachelvmadrigal@gmail.com>
2015-03-22 21:57:44 -0500

    Changed variable and method names to follow python naming conventions

--- bugdrop.py
+++ bugdrop.py
@@ -1,57 +1,78 @@
 import random
 
-NUMGAMECOLUMNS = 6
-COLUMNHEIGHT = 12
+NUM_GAME_COLUMNS = 6
+COLUMN_HEIGHT = 12
 
 
 class BugDrop(object):
     def __init__(self):
-        self.gameGrid = [[0 for x in range(COLUMNHEIGHT + 2)] for x in range(NUMGAMECOLUMNS)]      #+2 for drawing purposes
+        self.game_grid = [[0 for x in range(COLUMN_HEIGHT + 2)] for x in range(NUM_GAME_COLUMNS)]      #+2 for drawing purposes
         self.status = 'Playing'
-        self.colorMatchGrid = [[0 for x in range(COLUMNHEIGHT)] for x in range(NUMGAMECOLUMNS)]
     
     
-    def getColumnBase(self, column):
+    def get_column_base(self, column):
         base = 0
-        for item in range(len(self.gameGrid[column])):
-            if self.gameGrid[column][item] != 0:
+        for item in range(len(self.game_grid[column])):
+            if self.game_grid[column][item] != 0:
                 base = item + 1
         return base
             
             
-    def isAtMax(self, column):
-        if self.getColumnBase(column) == COLUMNHEIGHT:
+    def is_at_max(self, column):
+        if self.get_column_base(column) == COLUMN_HEIGHT:
             return True
         return False
                 
         
-    def addToColumn(self, color, column):
-        if column < NUMGAMECOLUMNS and column >= 0:
-            if self.isAtMax(column):
+    def add_to_column(self, color, column):
+        if column < NUM_GAME_COLUMNS and column >= 0:
+            if self.is_at_max(column):
                 self.status = 'Lost'
-            self.gameGrid[column][self.getColumnBase(column)] = color
+            self.game_grid[column][self.get_column_base(column)] = color
                     
                     
-    def dropBugSetInColumn(self, bugSet, column):
-        if bugSet.position == 'upPosition':
-            self.addToColumn(bugSet.bugColor1, column)
-            self.addToColumn(bugSet.bugColor2, column)
-        if bugSet.position == 'downPosition':
-            self.addToColumn(bugSet.bugColor2, column)
-            self.addToColumn(bugSet.bugColor1, column)
-        if bugSet.position == 'leftPosition':
-            self.addToColumn(bugSet.bugColor1, column)
-            self.addToColumn(bugSet.bugColor2, column - 1)
-        if bugSet.position == 'rightPosition':
-            self.addToColumn(bugSet.bugColor1, column)
-            self.addToColumn(bugSet.bugColor2, column + 1)  
+    def drop_bug_set_in_column(self, bug_set, column):
+        if bug_set.position == 'up_position':
+            self.add_to_column(bug_set.bug_color1, column)
+            self.add_to_column(bug_set.bug_color2, column)
+        if bug_set.position == 'down_position':
+            self.add_to_column(bug_set.bug_color2, column)
+            self.add_to_column(bug_set.bug_color1, column)
+        if bug_set.position == 'left_position':
+            self.add_to_column(bug_set.bug_color1, column)
+            self.add_to_column(bug_set.bug_color2, column - 1)
+        if bug_set.position == 'right_position':
+            self.add_to_column(bug_set.bug_color1, column)
+            self.add_to_column(bug_set.bug_color2, column + 1)
+        
+            
+    def recursive_series_check(self, column, row, series_count, passed):
+        if self.game_grid[column][row] == self.game_grid[column - 1][row] and (column, row) not in passed and column > 0:
+            passed.append((column, row))
+            series_count = self.recursive_series_check(column - 1, row, series_count + 1, passed)
+        if self.game_grid[column][row] == self.game_grid[column + 1][row] and (column, row) not in passed and column < NUM_GAME_COLUMNS - 2:
+            passed.append((column, row))
+            series_count = self.recursive_series_check(column + 1, row, series_count + 1, passed)
+        if self.game_grid[column][row] == self.game_grid[column][row - 1] and (column, row) not in passed and row > 0:
+            passed.append((column, row))
+            series_count = self.recursive_series_check(column, row - 1, series_count + 1, passed)
+        if self.game_grid[column][row] == self.game_grid[column][row + 1] and (column, row) not in passed and row > COLUMN_HEIGHT - 2:
+            passed.append((column, row))
+            series_count = self.recursive_series_check(column, row + 1, series_count + 1, passed)
+        return series_count
+        
+    
+    def count_adjacent_colors(self, column, row):
+        passed_cells = []
+        return self.recursive_series_check(column, row, 0, passed_cells)
+          
 
 
 class BugSet(object):
     def __init__(self):
-        self.bugColor1 = random.randint(1, 5)
-        self.bugColor2 = random.randint(1, 5)
-        self.position = 'upPosition'
+        self.bug_color1 = random.randint(1, 5)
+        self.bug_color2 = random.randint(1, 5)
+        self.position = 'up_position'
         self.location = 2
 
     
--- testbugdrop.py
+++ testbugdrop.py
@@ -3,84 +3,90 @@ from bugdrop import *
 
 class BugDropTest(unittest.TestCase):
     def setUp(self): 
-        self.bugDrop = BugDrop()
-        self.bugSet = BugSet()        
+        self.bug_drop = BugDrop()
+        self.bug_set = BugSet()        
         
-    def testcanary(self):
+    def test_canary(self):
         self.assertTrue(True)        
         
-    def testContentChangesColumnBase(self):
-        self.bugDrop.gameGrid[1][0] = 1
-        self.assertEquals(1, self.bugDrop.getColumnBase(1))    
+    def test_content_changes_column_base(self):
+        self.bug_drop.game_grid[1][0] = 1
+        self.assertEquals(1, self.bug_drop.get_column_base(1))    
         
-    def testIsAtMaxOnMaxedBaseReturnsTrue(self):
-        self.bugDrop.gameGrid[1][11] = 1       #GAMEHEIGHT - 1
-        self.assertTrue(self.bugDrop.isAtMax(1))        
+    def test_is_at_max_on_maxed_base_returns_true(self):
+        self.bug_drop.game_grid[1][11] = 1       #GAMEHEIGHT - 1
+        self.assertTrue(self.bug_drop.is_at_max(1))        
     
-    def testIsAtMaxOnLowBaseReturnsFalse(self):
-        self.bugDrop.gameGrid[1][3] = 1
-        self.assertFalse(self.bugDrop.isAtMax(1))    
+    def test_is_at_max_on_low_base_returns_false(self):
+        self.bug_drop.game_grid[1][3] = 1
+        self.assertFalse(self.bug_drop.is_at_max(1))    
         
-    def testAddToColumnChangesGameGrid(self):
-        self.bugDrop.addToColumn(1, 0)
-        self.assertEquals(1, self.bugDrop.gameGrid[0][0])        
+    def test_add_to_column_changes_game_grid(self):
+        self.bug_drop.add_to_column(1, 0)
+        self.assertEquals(1, self.bug_drop.game_grid[0][0])        
     
-    def testAddToColumnForInvalidColumnDoesNothing(self):
-        self.bugDrop.addToColumn(1, -1)
+    def test_add_to_column_for_invalid_column_does_nothing(self):
+        self.bug_drop.add_to_column(1, -1)
         total = 0
         for x in range(6):
-            total += self.bugDrop.gameGrid[x][0]
+            total += self.bug_drop.game_grid[x][0]
         self.assertEquals(0, total)  
         
-    def testAddToColumnForMaxedColumnLosesGame(self):
-        self.bugDrop.gameGrid[1][11] = 1
-        self.bugDrop.addToColumn(1, 1)
-        self.assertEquals('Lost', self.bugDrop.status)       
+    def test_add_to_column_for_maxed_column_loses_game(self):
+        self.bug_drop.game_grid[1][11] = 1
+        self.bug_drop.add_to_column(1, 1)
+        self.assertEquals('Lost', self.bug_drop.status)       
     
-    def testAddToColumnIncreasesBase(self):
-        self.bugDrop.addToColumn(1, 1)
-        self.assertEquals(1, self.bugDrop.getColumnBase(1))        
+    def test_add_to_column_increases_base(self):
+        self.bug_drop.add_to_column(1, 1)
+        self.assertEquals(1, self.bug_drop.get_column_base(1))        
         
-    def testDropUpBugSetInColumnChangesGameGrid(self):
-        self.bugDrop.dropBugSetInColumn(self.bugSet, 1)
-        self.assertEquals(self.bugDrop.gameGrid[1][1], self.bugSet.bugColor2)
+    def test_drop_up_bug_set_in_column_changes_game_grid(self):
+        self.bug_drop.drop_bug_set_in_column(self.bug_set, 1)
+        self.assertEquals(self.bug_drop.game_grid[1][1], self.bug_set.bug_color2)
         
-    def testDropDownBugSetInColumnChangesGameGrid(self):
-        self.bugSet.position = 'downPosition'
-        self.bugDrop.dropBugSetInColumn(self.bugSet, 1)
-        self.assertEquals(self.bugDrop.gameGrid[1][1], self.bugSet.bugColor1)
+    def test_drop_down_bug_set_in_column_changes_game_grid(self):
+        self.bug_set.position = 'down_position'
+        self.bug_drop.drop_bug_set_in_column(self.bug_set, 1)
+        self.assertEquals(self.bug_drop.game_grid[1][1], self.bug_set.bug_color1)
         
-    def testDropVerticalBugSetInFullColumnLoses(self):
-        self.bugDrop.gameGrid[1][10] = 1
-        self.bugDrop.dropBugSetInColumn(self.bugSet, 1)
-        self.assertTrue('Lost', self.bugDrop.status)
+    def test_drop_vertical_bug_set_in_full_column_loses(self):
+        self.bug_drop.game_grid[1][10] = 1
+        self.bug_drop.drop_bug_set_in_column(self.bug_set, 1)
+        self.assertTrue('Lost', self.bug_drop.status)
         
-    def testDropLeftBugSetInColumnChangesGameGrid(self):
-        self.bugSet.position = 'leftPosition'
-        self.bugDrop.dropBugSetInColumn(self.bugSet, 5)
-        self.assertEquals(self.bugDrop.gameGrid[4][0], self.bugSet.bugColor2)
+    def test_drop_left_bug_set_in_column_changes_game_grid(self):
+        self.bug_set.position = 'left_position'
+        self.bug_drop.drop_bug_set_in_column(self.bug_set, 5)
+        self.assertEquals(self.bug_drop.game_grid[4][0], self.bug_set.bug_color2)
         
-    def testDropRightBugSetInColumnChangesGameGrid(self):
-        self.bugSet.position = 'rightPosition'
-        self.bugDrop.dropBugSetInColumn(self.bugSet, 3)
-        self.assertEquals(self.bugDrop.gameGrid[4][0], self.bugSet.bugColor2)
+    def test_drop_right_bug_set_in_column_changes_game_grid(self):
+        self.bug_set.position = 'right_position'
+        self.bug_drop.drop_bug_set_in_column(self.bug_set, 3)
+        self.assertEquals(self.bug_drop.game_grid[4][0], self.bug_set.bug_color2)
         
-    def testDropInvalidPositionBugSetInColumnDoesNothing(self):
-        self.bugSet.position = 'blah'
-        self.bugDrop.dropBugSetInColumn(self.bugSet, 0)
-        self.assertEquals(0, self.bugDrop.gameGrid[0][0])
+    def test_drop_invalid_position_bug_set_in_column_does_nothing(self):
+        self.bug_set.position = 'blah'
+        self.bug_drop.drop_bug_set_in_column(self.bug_set, 0)
+        self.assertEquals(0, self.bug_drop.game_grid[0][0])
+        
+    def test_count_adjacent_color_returns_correct_number_of_adjacent_colors(self):
+        self.bug_drop.game_grid[2][0] = 1
+        self.bug_drop.game_grid[3][0] = 1
+        self.bug_drop.game_grid[4][0] = 1
+        self.assertEquals(2, self.bug_drop.count_adjacent_colors(3, 0))
         
         
-class BugSetTest(unittest.TestCase):
+class bug_setTest(unittest.TestCase):
     def setUp(self):
-        self.bugSet = BugSet()
+        self.bug_set = BugSet()
         
-    def testCanary(self):
+    def test_canary(self):
         self.assertTrue(True)
         
-    def testInitialBugSetCreatesRandomSets(self):
+    def test_initial_bug_set_creates_random_sets(self):
         random = BugSet()
-        self.assertTrue(self.bugSet != random)
+        self.assertTrue(self.bug_set != random)
 
         
 if __name__ == '__main__':
