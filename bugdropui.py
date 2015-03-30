042ded33375ffa6a437a37a6376ecdcf2be8caaf
rachelmad <rachelvmadrigal@gmail.com>
2015-03-29 22:18:21 -0500

    Implemented Sprites

--- bugdropui.py
+++ bugdropui.py
@@ -14,6 +14,10 @@ def main():
     
     while True:
         ui.main_frame.fill(WHITE)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
         pygame.display.update()
         pygame.time.Clock().tick(100)
 
@@ -21,14 +25,19 @@ def main():
 class BugDropUI(object):
     def __init__(self, width, height):
         self.main_frame = pygame.display.set_mode((width, height))
-     
+        self.all_sprites = pygame.sprite.Group()
+        self.all_bugs = pygame.sprite.Group()
+        
+        #add game frame
+        #all_sprites_list.add(block)
         
 class Bug(pygame.sprite.Sprite):
     def __init__(self, color):
         super().__init__(self)
+        self.image = pygame.Surface((BUG_SIZE, BUG_SIZE))
+        pygame.draw.circle(self.image, color, (0, 0), BUG_SIZE / 2)
+        self.rect = self.image.get_rect()
         
 if __name__ == '__main__':
     main()
     
-    
-                    # LEARN ABOUT SPRITES!!!
