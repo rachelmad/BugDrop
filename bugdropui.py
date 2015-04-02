5b988cbbbb4ce5aafe351b34ae4538d5213d958d
rachelmad <rachelvmadrigal@gmail.com>
2015-04-01 21:48:49 -0500

    Created create_new_bugset function

--- bugdropui.py
+++ bugdropui.py
@@ -7,6 +7,11 @@ BUG_SIZE = 34
 
 WHITE = ( 255, 255, 255)
 BLACK = ( 0, 0, 0)
+RED = ( 255, 0, 0)
+GREEN = ( 0, 255, 0)
+BLUE = ( 0, 0, 255)
+PURPLE = ( 255, 0, 255)
+YELLOW = ( 255, 255, 0)
 
 def main():
     pygame.init()
@@ -18,15 +23,21 @@ def main():
         
         
 def play_game(ui):
-    ui.main_frame.fill(WHITE)
-    ui.make_walls()
-    ui.game_frame.draw(ui.main_frame)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
-    pygame.time.Clock().tick(100)
+    next_drop = ui.create_new_bugset()
+    
+    while True:
+        ui.main_frame.fill(WHITE)
+        ui.make_walls()
+        ui.game_frame.draw(ui.main_frame)
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        next_drop.draw(ui.main_frame)
+        pygame.display.update()
+        pygame.time.Clock().tick(100)
+    
 
 
 class BugDropUI(object):
@@ -38,33 +49,69 @@ class BugDropUI(object):
         self.all_sprites = pygame.sprite.Group()
         self.all_bugs = pygame.sprite.Group()
         
+        self.game_frame_width = BUG_SIZE * self.bugdrop.num_columns
+        self.game_frame_height = BUG_SIZE * self.bugdrop.column_height
+        self.top_left_position_x = (3 * MAIN_WIDTH / 4) - self.game_frame_width / 2
+        self.top_left_position_y = BUG_SIZE * 3
         
-    def make_walls(self):
-        game_frame_width = BUG_SIZE * self.bugdrop.num_columns
-        game_frame_height = BUG_SIZE * self.bugdrop.column_height
-        top_left_position_x = (3 * MAIN_WIDTH / 4) - game_frame_width / 2
-        top_left_position_y = BUG_SIZE * 3
         
-        left_wall = Wall(5, game_frame_height)
-        left_wall.rect.x = top_left_position_x - 5
-        left_wall.rect.y = top_left_position_y
-        bottom_wall = Wall(game_frame_width + 10, 5)
-        bottom_wall.rect.x = top_left_position_x - 5
-        bottom_wall.rect.y = top_left_position_y + game_frame_height
-        right_wall = Wall(5, game_frame_height)
-        right_wall.rect.x = top_left_position_x + game_frame_width
-        right_wall.rect.y = top_left_position_y
+    def make_walls(self):        
+        left_wall = Wall(5, self.game_frame_height)
+        left_wall.rect.x = self.top_left_position_x - 5
+        left_wall.rect.y = self.top_left_position_y
+        bottom_wall = Wall(self.game_frame_width + 10, 5)
+        bottom_wall.rect.x = self.top_left_position_x - 5
+        bottom_wall.rect.y = self.top_left_position_y + self.game_frame_height
+        right_wall = Wall(5, self.game_frame_height)
+        right_wall.rect.x = self.top_left_position_x + self.game_frame_width
+        right_wall.rect.y = self.top_left_position_y
         
         self.game_frame.add(left_wall)
         self.game_frame.add(bottom_wall)
         self.game_frame.add(right_wall)
+        
+
+    def create_new_bugset(self):
+        bugset = BugSet()
+        if bugset.bug_color1 == 1:
+            bug1 = Bug(RED)
+        elif bugset.bug_color1 == 2:
+            bug1 = Bug(GREEN)
+        elif bugset.bug_color1 == 3:
+            bug1 = Bug(BLUE)
+        elif bugset.bug_color1 == 4:
+            bug1 = Bug(PURPLE)
+        elif bugset.bug_color1 == 5:
+            bug1 = Bug(YELLOW)
+            
+        if bugset.bug_color2 == 1:
+            bug2 = Bug(RED)
+        elif bugset.bug_color2 == 2:
+            bug2 = Bug(GREEN)
+        elif bugset.bug_color2 == 3:
+            bug2 = Bug(BLUE)
+        elif bugset.bug_color2 == 4:
+            bug2 = Bug(PURPLE)
+        elif bugset.bug_color2 == 5:
+            bug2 = Bug(YELLOW)
+                
+        bug1.rect.x = self.top_left_position_x + BUG_SIZE * 2
+        bug1.rect.y = self.top_left_position_y - BUG_SIZE
+        bug2.rect.x = self.top_left_position_x + BUG_SIZE * 2
+        bug2.rect.y = self.top_left_position_y - BUG_SIZE * 2
+        
+        bugset_sprites = pygame.sprite.Group()
+        bugset_sprites.add(bug1)
+        bugset_sprites.add(bug2)
+        return bugset_sprites
      
         
 class Bug(pygame.sprite.Sprite):
     def __init__(self, color):
         super(Bug, self).__init__()
         self.image = pygame.Surface((BUG_SIZE, BUG_SIZE))
-        pygame.draw.circle(self.image, color, (0, 0), BUG_SIZE / 2)
+        self.image.fill(WHITE)
+        pygame.draw.circle(self.image, color, (BUG_SIZE / 2, BUG_SIZE / 2), BUG_SIZE / 2)
         self.rect = self.image.get_rect()
         
         
